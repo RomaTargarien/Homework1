77c4d5506fbaf15dcdabd4bb82c044b336cbdcf0
Roma Tolstikov <tolstikovski@mai.ru>
2020-09-02 12:49:20 +0300

    Third commit

--- hometask1/task5n1.java
+++ hometask1/task5n1.java
@@ -3,16 +3,21 @@ package hometask1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class task5n1 {
     public static void main(String[] args) throws IOException  {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name = reader.readLine();
-        if (name.toLowerCase().equals("вася")){
+        String name = (reader.readLine()).toLowerCase();
+        boolean boyName = Objects.equals(name,"вася");
+        boolean girlName = Objects.equals(name,"анастасия");
+        if (boyName){
             System.out.println("Привет \nЯ тебя так долго ждал");
+            return;
         }
-        else if (name.toLowerCase().equals("анастасия")){
+        else if (girlName){
             System.out.println("Я тебя так долго ждал");
+            return;
         } else  {
             System.out.println("Добрый день, а вы кто?");
         }
--- hometask1/task5n3.java
+++ hometask1/task5n3.java
@@ -3,21 +3,23 @@ package hometask1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class task5n3 {
     public static void main(String[] args)throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name = reader.readLine();
-        if (name.toLowerCase().equals("вася") || name.toLowerCase().equals("анастасия")){
-            if (name.toLowerCase().equals("вася")){
+        String name = (reader.readLine()).toLowerCase();
+        boolean boyName = Objects.equals(name,"вася");
+        boolean girlName = Objects.equals(name,"анастасия");
+        if (boyName || girlName){
+            if (boyName){
                 System.out.println("Привет \nЯ тебя так долго ждал");
-            }
-            else {
+            } if (girlName){
                 System.out.println("Я тебя так долго ждал");
             }
         }
-        else {
-            System.out.println("Добрый день, а вы кто?");
+        if (!boyName && !girlName){
+            System.out.print("Добрый день, а вы кто?");
         }
     }
 }
